cb0e923cda1bf4ca7c1b9c74221097b17ee0c414
Juan Ignacio <juanignacioalemani@gmail.com>
2021-03-31 17:55:45 -0300

    agregaron paletas y el volvieron controlables

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from turtle import Screen
-
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor('black')
 screen.setup(800,600)
 screen.title("Pong")
+screen.tracer(0)
+
+paddle_1 = Paddle((350,0))
+paddle_2 = Paddle((-350,0))
+
+screen.listen()
+screen.onkey(paddle_1.go_up,key="Up")
+screen.onkey(paddle_1.go_down,key="Down")
+screen.onkey(paddle_2.go_up,key="w")
+screen.onkey(paddle_2.go_down,key="s")
+is_on = True
+while is_on == True:
+    screen.update()
 
 
 
